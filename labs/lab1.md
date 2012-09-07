1daba5b0e3e4c9d071e442fa6c403fecd671287a
Michael Morris-Pearce <mikemp@mit.edu>
2012-09-06 22:27:52 -0400

    Added lab1 skeleton

---
+++ labs/lab1.md
@@ -0,0 +1,19 @@
+Python Laboratory
+=================
+
+1. Welcome to Python!
+
+2. Getting Started
+
+3. Turtle Graphics
+
+    import turtle
+    
+    turtle.forward(100)
+    turtle.left(
+
+4. 
+
+5. Challenge
+
+
---
+++ labs/lab1.py
@@ -0,0 +1,25 @@
+import turtle
+
+# Draw a square.
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+
+# Draw a square whenever you type "square()".
+def square():
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+    turtle.forward(200)
+    turtle.left(90)
+
+# Draw a square.
+square()
