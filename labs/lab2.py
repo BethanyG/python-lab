bdefd2d0c02e27494f38ef542600efc24f063225
Michael Morris-Pearce <mikemp@mit.edu>
2012-09-07 02:08:49 -0600

    Update labs/lab2.py

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,5 +1,10 @@
 import turtle
 
+colors = ["black"]
+
+def colors(iteration):
+    turtle.pencolor(colors[iteration % len(colors)])
+
 def spirograph():
     for iteration in range(1, 1000):    
         # Example (REPLACE WITH YOUR OWN DESIGN):
