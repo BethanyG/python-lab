551e786ed9027b4f860c90b91646c152994db88e
Michael Morris-Pearce <mikemp@mit.edu>
2012-09-07 01:56:55 -0600

    Update labs/lab2.py

--- labs/lab2.py
+++ labs/lab2.py
@@ -3,3 +3,7 @@ import turtle
 def spirograph():
     for iteration in range(1, 1000):
         pass # DELETE THIS LINE AND REPLACE WITH YOUR CODE
+        
+        # Example:
+        #turtle.line(300)
+        #turtle.right(170)
