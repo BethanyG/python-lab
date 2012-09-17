df00b6a5fc663c683736d2d0b0a97b449e0bd360
Michael Morris-Pearce <mikemp@mit.edu>
2012-09-16 21:35:24 -0600

    Update labs/lab2.py

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,12 +1,72 @@
 import turtle
+from math import sin, pi
 
-colors = ["black"]
+def square():
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
+    turtle.right(90)
+
+def heart(size, shape):
+    turtle.pensize(10)
+    radius = size * sin(shape * pi / 180) / (1 + sin((90 - shape) * pi / 180))
+    turtle.right(shape)
+    turtle.forward(size)
+    turtle.circle(radius, 180 + shape)
+    turtle.right(180)
+    turtle.circle(radius, 180 + shape)
+    turtle.forward(size)
+    turtle.left(180 - shape)
+
+def at(x, y):
+    turtle.penup()
+    turtle.home()
+    turtle.forward(x)
+    turtle.left(90)
+    turtle.forward(y)
+    turtle.pendown()
+
+def wheel():
+    turtle.pensize(10)
+    turtle.pencolor("black")
+    turtle.right(90)
+    turtle.circle(50)
+    turtle.left(90)
+
+def chassis():
+    heart(200, 60)
+
+def car():
+    at(150, 0)
+    wheel()
+    at(-150, 0)
+    wheel()
+    at(0, 25)
+    chassis()
+
+#### Now for something completely different ####
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple", "violet"]
 
 def color(iteration):
     turtle.pencolor(colors[iteration % len(colors)])
 
 def spirograph():
-    for iteration in range(1, 1000):    
-        # Example (REPLACE WITH YOUR OWN DESIGN):
-        turtle.forward(300)
-        turtle.right(170)
+    turtle.delay(0)
+    turtle.pensize(3)
+    at(0, -200)
+    for iteration in range(1, 100):
+        color(iteration)
+        turtle.circle(400, 80)
+        turtle.circle(50, 175)
+
+def hearts():
+    turtle.delay(0)
+    for iteration in range(1, 50):
+        color(iteration)
+        at(0, iteration * -5)
+        heart(iteration * 10, 45)
